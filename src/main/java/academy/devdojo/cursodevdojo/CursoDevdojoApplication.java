fd8d22524711ded4acee674696cdd04073d9e11d
viniCentorbi <vinicentorbi@hotmail.com>
2022-08-04 13:36:21 -0300

    Projeto base

---
+++ src/main/java/academy/devdojo/cursodevdojo/CursoDevdojoApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.cursodevdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDevdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDevdojoApplication.class, args);
+	}
+
+}
