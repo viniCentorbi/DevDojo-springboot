3748812a83330a8d4ae6a1d1601038dd3cb631bd
viniCentorbi <vinicentorbi@hotmail.com>
2022-08-08 13:22:17 -0300

    Implemented DELETE method

--- src/main/java/academy/devdojo/cursodevdojo/controller/AnimeController.java
+++ src/main/java/academy/devdojo/cursodevdojo/controller/AnimeController.java
@@ -37,4 +37,14 @@ public class AnimeController {
     public ResponseEntity<Anime> save(@RequestBody Anime anime){
         return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id){
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        //maneira que eu pensei em fazer
+        //animeService.delete(id);
+        //return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
 }
