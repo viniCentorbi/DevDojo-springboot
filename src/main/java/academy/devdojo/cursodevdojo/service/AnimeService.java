98bf34289478a2a2df4a0609dc5bb0e3a29c897d
viniCentorbi <vinicentorbi@hotmail.com>
2022-08-08 12:52:25 -0300

    Implemented POST method

--- src/main/java/academy/devdojo/cursodevdojo/service/AnimeService.java
+++ src/main/java/academy/devdojo/cursodevdojo/service/AnimeService.java
@@ -5,12 +5,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime(1L, "One Piece"), new Anime(2L,"One Punch Man"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "One Piece"), new Anime(2L,"One Punch Man")));
+    }
+
     public List<Anime> listAll(){
         return animes;
     }
@@ -20,5 +27,12 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        animes.add(anime);
+
+        return anime;
     }
 }
