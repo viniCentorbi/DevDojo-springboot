ec4e832c1b0f29cb8c7bbcaf5d324361e085328d
viniCentorbi <vinicentorbi@hotmail.com>
2022-08-08 13:46:21 -0300

    Implemented PUT method

--- src/main/java/academy/devdojo/cursodevdojo/controller/AnimeController.java
+++ src/main/java/academy/devdojo/cursodevdojo/controller/AnimeController.java
@@ -42,9 +42,13 @@ public class AnimeController {
     public ResponseEntity<Void> delete(@PathVariable long id){
         animeService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-
         //maneira que eu pensei em fazer
-        //animeService.delete(id);
         //return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime){
+        animeService.replace(anime);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
