a82b107909dc51e69feabcb4873bc37d0e1d09c3
viniCentorbi <vinicentorbi@hotmail.com>
2022-08-09 11:20:05 -0300

    Added database connection with Spring Data JPA

--- src/main/java/academy/devdojo/cursodevdojo/domain/Anime.java
+++ src/main/java/academy/devdojo/cursodevdojo/domain/Anime.java
@@ -2,10 +2,22 @@ package academy.devdojo.cursodevdojo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data //gera getters, setters, equals and hashcodes
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+
 }
