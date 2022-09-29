92ae017d30bd74fdd84f134b29e9f79dba32b50d
vinicius-centorbi <vinicius.centorbi@socin.com.br>
2022-09-29 13:17:42 -0300

    Added a method for list all animes non using paging

--- src/main/java/academy/devdojo/cursodevdojo/client/SpringClient.java
+++ src/main/java/academy/devdojo/cursodevdojo/client/SpringClient.java
@@ -2,8 +2,12 @@ package academy.devdojo.cursodevdojo.client;
 
 import academy.devdojo.cursodevdojo.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -13,5 +17,8 @@ public class SpringClient {
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(object);
+
+        ResponseEntity<List<Anime>> animes = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {});
+        log.info(animes);
     }
 }
