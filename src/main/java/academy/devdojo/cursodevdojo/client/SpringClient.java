66f5af1957b6a243f71694fbdecff880ade43b16
vinicius-centorbi <vinicius.centorbi@socin.com.br>
2022-09-29 13:45:33 -0300

    Using RestTemplate exchange to make POST requests for anime

--- src/main/java/academy/devdojo/cursodevdojo/client/SpringClient.java
+++ src/main/java/academy/devdojo/cursodevdojo/client/SpringClient.java
@@ -3,6 +3,7 @@ package academy.devdojo.cursodevdojo.client;
 import academy.devdojo.cursodevdojo.domain.Anime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -12,13 +13,26 @@ import java.util.List;
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
+
+        //Buscar anime
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(entity);
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 2);
         log.info(object);
 
-        ResponseEntity<List<Anime>> animes = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {});
+        //Listar animes
+        ResponseEntity<List<Anime>> animes = new RestTemplate().exchange("http://localhost:8080/animes/all",
+                                                                            HttpMethod.GET,
+                                                                null,
+                                                                            new ParameterizedTypeReference<List<Anime>>() {});
         log.info(animes);
+
+        //Inserir animes
+        Anime jujutsuKaisen = Anime.builder().name("Jujutsu Kaisen").build();
+        ResponseEntity<Anime> jujutsuKaisenSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+                                                                                HttpMethod.POST,
+                                                                                new HttpEntity<>(jujutsuKaisen),
+                                                                                Anime.class);
     }
 }
